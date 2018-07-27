f02c2e0f3e92f782cb612ac222137285765f3c2d
starbugs <1457896625@qq.com>
2018-07-27 17:12:15 +0800

    update i18n

--- src/i18n/form.js
+++ src/i18n/form.js
@@ -2,6 +2,7 @@ const zh = {
   form_error: '程序错误',
   form_nickname_empty: '用户名不能为空',
   form_account_empty: '账号不能为空',
+  form_account_length: '长度为2-12',
   form_account_unregistered: '该账号未注册',
 
   form_pass_empty: '登录密码不能为空',
@@ -15,21 +16,31 @@ const zh = {
   form_vcode_input: '请输入验证码',
   form_vcode_err: '验证码不正确',
   form_vcode_empty: '验证码不能为空',
+  form_vcode_send: '验证码发送成功',
 
   form_cell_empty: '手机号不能为空',
   form_cell_input: '请输入正确的手机号',
   form_cell_reged: '该手机号已注册',
+  form_cell_nreged: '该手机号未注册',
 
   form_email_reged: '该邮箱已注册',
+  form_email_nreged: '该邮箱未注册',
   form_email_format: '邮箱格式不正确',
   form_email_empty: '邮箱不能为空',
   form_email_valid: '您输入的邮箱格式不正确',
 
   form_fpass_success: '修改资金密码成功',
+  form_fpass_correct: '资金密码不正确',
+  form_fpass_set: '请先设置资金密码',
 
   form_google_code_empty: '谷歌验证码不能为空',
   form_google_code_wrong: '谷歌验证码不正确',
   form_google_success: '谷歌认证成功',
+
+  form_addr_empty: '地址不能为空',
+  form_take_empty: '提取金额不能为空',
+  form_take_min: '提币金额低于最小提币金额',
+  form_take_enough: '资金不足',
 }
 
 
@@ -37,6 +48,7 @@ const zw = {
   form_error: '程式錯誤',
   form_nickname_empty: '用戶名不能為空',
   form_account_empty: '帳號不能為空',
+  form_account_length: '長度為2-12',
   form_account_unregistered: '該帳號未注册',
 
   form_pass_empty: '登入密碼不能為空',
@@ -50,26 +62,38 @@ const zw = {
   form_vcode_input: '請輸入驗證碼',
   form_vcode_err: '請輸入不正確',
   form_vcode_empty: '驗證碼不能為空',
+  form_vcode_send: '驗證碼發送成功',
+
   form_cell_empty: '手機號不能為空',
   form_cell_input: '請輸入正確的手機號',
   form_cell_reged: '該手機號已注册',
+  form_cell_nreged: '該手機號未注册',
 
   form_email_reged: '該郵箱已注册',
+  form_email_nreged: '該郵箱未注册',
   form_email_format: '郵箱格式不正確',
   form_email_empty: '郵箱不能為空',
   form_email_valid: '您輸入的郵箱格式不正確',
 
   form_fpass_success: '修改資金密碼成功',
+  form_fpass_correct: '資金密碼不正確',
+  form_fpass_set: '請先設定資金密碼',
 
   form_google_code_empty: '穀歌驗證碼不能為空',
   form_google_code_wrong: '穀歌驗證碼不正確',
   form_google_success: '穀歌認證成功',
+
+  form_addr_empty: '地址不能為空',
+  form_take_empty: '選取金額不能為空',
+  form_take_min: '提幣金額低於最小提幣金額',
+  form_take_enough: '資金不足',
 }
 
 const en = {
   form_error: 'System error',
   form_nickname_empty: 'The username should not be empty',
   form_account_empty: 'Account should be empty',
+  form_account_length: 'The user name length is 2-12',
   form_account_unregistered: 'The account is not registered',
 
   form_pass_empty: 'The login password should not be empty',
@@ -83,21 +107,32 @@ const en = {
   form_vcode_input: 'Please enter the verification code',
   form_vcode_err: 'Validation code is incorrect',
   form_vcode_empty: 'Validation code should not be empty',
+  form_vcode_send: 'The verification code was sent successfully',
+
   form_cell_empty: 'Cell phone Numbers should not be empty',
   form_cell_input: 'Please enter the correct mobile phone number',
   form_cell_reged: 'The phone number has been registered',
+  form_cell_nreged: 'The phone number is not registered',
 
 
   form_email_reged: 'The mailbox is registered',
+  form_email_nreged: 'The mailbox is not registered',
   form_email_format: 'The mailbox format is incorrect',
   form_email_empty: 'Email should not be empty',
   form_email_valid: 'The email you entered is incorrectly formatted',
 
   form_fpass_success: 'Successful modification of funds password',
+  form_fpass_correct: 'Incorrect fund password',
+  form_fpass_set: 'Please set the fund password first',
 
   form_google_code_empty: 'The Google verification code cannot be empty',
   form_google_code_wrong: 'The Google verification code is incorrect',
   form_google_success: 'successful',
+
+  form_addr_empty: 'The address cannot be empty',
+  form_take_empty: 'The withdrawal amount cannot be empty',
+  form_take_min: 'The amount of withdrawal is lower than the minimum amount of withdrawal',
+  form_take_enough: 'Insufficient funds',
 }
 
 export default {
